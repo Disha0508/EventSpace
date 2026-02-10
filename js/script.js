4efb683ed9302f243737b131a69def1163da973f
Diana <Dianasha2006@gmail.com>
2026-02-10 20:39:58 +0300

    Initial commit: project structure

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Проверка, что скрипт подключён
+console.log("Script.js подключён успешно");
+
+// Небольшое приветствие при загрузке страницы
+document.addEventListener("DOMContentLoaded", () => {
+    alert("Добро пожаловать в EventSpace!");
+});
